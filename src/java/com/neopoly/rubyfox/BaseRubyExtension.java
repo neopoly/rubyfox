57a0bcadc4bf7cc8172c9ff742b89050b6b2dad3
Peter Suschlik <ps@neopoly.de>
2012-11-06 12:21:43 +0100

    Add event and request logging.

---
+++ src/java/com/neopoly/rubyfox/BaseRubyExtension.java
@@ -0,0 +1,88 @@
+package com.neopoly.rubyfox;
+
+import com.smartfoxserver.v2.core.ISFSEvent;
+import com.smartfoxserver.v2.core.SFSEventType;
+import com.smartfoxserver.v2.entities.User;
+import com.smartfoxserver.v2.entities.data.ISFSObject;
+import com.smartfoxserver.v2.exceptions.SFSException;
+import com.smartfoxserver.v2.extensions.BaseServerEventHandler;
+import com.smartfoxserver.v2.extensions.SFSExtension;
+
+public class BaseRubyExtension extends SFSExtension {
+    private JRuby _jruby;
+
+    public void init() {
+        logBanner();
+
+        installFakeEventHandlers();
+
+        _jruby = new JRuby(this);
+        _jruby.load();
+        _jruby.handleInit();
+    }
+
+    @Override
+    public void handleClientRequest(String requestId, User sender, ISFSObject params) {
+        _jruby.handleClientRequest(requestId, sender, params);
+    }
+
+    @Override
+    public void handleServerEvent(ISFSEvent event) throws Exception {
+        _jruby.handleServerEvent(event);
+    }
+
+    public String getConfigProperty(String name) {
+        return getConfigProperties().getProperty(name);
+    }
+
+    @Override
+    public void destroy() {
+        _jruby.handleDestroy();
+        trace("Destroying " + this);
+        super.destroy();
+    }
+
+    private void logBanner() {
+        trace("Booting " + this);
+        trace("  Name: " + getName());
+        trace("  Dir: " + getCurrentFolder());
+        trace("  Extension filename: " + getExtensionFileName() + " (" + getLevel() + ")");
+        traceLevel();
+        trace("  Reload mode: " + getReloadMode());
+        trace("  Config file: " + getPropertiesFileName());
+        trace("  Config properties: " + getConfigProperties());
+    }
+
+    private void traceLevel() {
+        switch (getLevel()) {
+            case ZONE:
+                trace("  Zone: " + getParentZone());
+                break;
+            case ROOM:
+                trace("  Room: " + getParentRoom());
+                break;
+            case GLOBAL:
+                trace("  Global");
+                break;
+        }
+    }
+
+    /**
+     * Install fake handlers for all {@linkplain com.smartfoxserver.v2.core.SFSEvent event types}.
+     *
+     * None of these events will be handled by the fake handler. {@linkplain #handleServerEvent(com.smartfoxserver.v2.core.ISFSEvent)} handles them.
+     * We need to register them all to tell SmartFox to fire all events.
+     */
+    private void installFakeEventHandlers() {
+        for (SFSEventType eventType : SFSEventType.values()) {
+            addEventHandler(eventType, FakeHandler.class);
+        }
+    }
+
+    public static class FakeHandler extends BaseServerEventHandler {
+        @Override
+        public void handleServerEvent(ISFSEvent event) throws SFSException {
+            throw new SFSException("not used");
+        }
+    }
+}
--- src/java/com/neopoly/rubyfox/RubyExtension.java
+++ src/java/com/neopoly/rubyfox/RubyExtension.java
@@ -1,84 +1,108 @@
 package com.neopoly.rubyfox;
 
 import com.smartfoxserver.v2.core.ISFSEvent;
-import com.smartfoxserver.v2.core.SFSEventType;
+import com.smartfoxserver.v2.core.SFSEventParam;
+import com.smartfoxserver.v2.entities.Room;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
-import com.smartfoxserver.v2.exceptions.SFSException;
-import com.smartfoxserver.v2.extensions.BaseServerEventHandler;
-import com.smartfoxserver.v2.extensions.SFSExtension;
+import com.smartfoxserver.v2.entities.data.SFSObject;
 
-public class RubyExtension extends SFSExtension {
-    private JRuby _jruby;
+import java.util.*;
 
+public class RubyExtension extends BaseRubyExtension {
+    private static final Set<String> DONT_ACCUMULATE = new HashSet<String>();
+
+    @Override
     public void init() {
-        logBanner();
-
-        installFakeEventHandlers();
-
-        _jruby = new JRuby(this);
-        _jruby.load();
-        _jruby.handleInit();
+        super.init();
+        ignoreAccumulates();
     }
 
     @Override
     public void handleClientRequest(String requestId, User sender, ISFSObject params) {
-        _jruby.handleClientRequest(requestId, sender, params);
+        long start = System.currentTimeMillis();
+        super.handleClientRequest(requestId, sender, params);
+        long duration = System.currentTimeMillis() - start;
+        accumulate(requestId, sender, params, duration);
     }
 
     @Override
     public void handleServerEvent(ISFSEvent event) throws Exception {
-        _jruby.handleServerEvent(event);
+        long start = System.currentTimeMillis();
+        super.handleServerEvent(event);
+        long duration = System.currentTimeMillis() - start;
+        accumulate(event, duration);
     }
 
     @Override
-    public void destroy() {
-        _jruby.handleDestroy();
-        trace("Destroying " + this);
-        super.destroy();
+    public void send(String cmdName, ISFSObject params, List<User> recipients, boolean useUDP) {
+        if (recipients.isEmpty()) return;
+        accumulate(cmdName, recipients, params, useUDP);
+        super.send(cmdName, params, recipients, useUDP);
     }
 
-    private void logBanner() {
-        trace("Booting " + this);
-        trace("  Name: " + getName());
-        trace("  Dir: " + getCurrentFolder());
-        trace("  Extension filename: " + getExtensionFileName() + " (" + getLevel() + ")");
-        traceLevel();
-        trace("  Reload mode: " + getReloadMode());
-        trace("  Config file: " + getPropertiesFileName());
-        trace("  Config properties: " + getConfigProperties());
+    @Override
+    public void send(String cmdName, ISFSObject params, User recipient, boolean useUDP) {
+        accumulate(cmdName, recipient, params, useUDP);
+        super.send(cmdName, params, recipient, useUDP);
     }
 
-    private void traceLevel() {
-        switch (getLevel()) {
-            case ZONE:
-                trace("  Zone: " + getParentZone());
-                break;
-            case ROOM:
-                trace("  Room: " + getParentRoom());
-                break;
-            case GLOBAL:
-                trace("  Global");
-                break;
+    // ---------------------------------------------------------
+
+    private void accumulate(ISFSEvent event, long duration) {
+        User user = (User) event.getParameter(SFSEventParam.USER);
+        Room room = (Room) event.getParameter(SFSEventParam.ROOM);
+        String sender = user != null ? user.getName() : room != null ? room.getName() : "?";
+
+        SFSObject params = extractParameters(event);
+        String log = String.format("EVNT[%s]: %s(%s) in %dms", sender, event.getType().name(), params.toJson(), duration);
+        trace(log);
+    }
+
+    private SFSObject extractParameters(ISFSEvent event) {
+        SFSObject params = new SFSObject();
+        for (SFSEventParam param : SFSEventParam.values()) {
+            switch (param) {
+                case ZONE:
+                case USER:
+                    break;
+                default:
+                    Object value = event.getParameter(param);
+                    if (value != null)
+                        params.putUtfString(param.name(), String.valueOf(value));
+            }
         }
+        return params;
     }
 
-    /**
-     * Install fake handlers for all {@linkplain com.smartfoxserver.v2.core.SFSEvent event types}.
-     *
-     * None of these events will be handled by the fake handler. {@linkplain #handleServerEvent(com.smartfoxserver.v2.core.ISFSEvent)} handles them.
-     * We need to register them all to tell SmartFox to fire all events.
-     */
-    private void installFakeEventHandlers() {
-        for (SFSEventType eventType : SFSEventType.values()) {
-            addEventHandler(eventType, FakeHandler.class);
-        }
+    private void accumulate(String requestId, User sender, ISFSObject params, long duration) {
+        if (ignoreAccumulate(requestId)) return;
+        String log = String.format("RECV[%s]: %s(%s) in %dms", sender.getName(), requestId, params.toJson(), duration);
+        trace(log);
     }
 
-    public static class FakeHandler extends BaseServerEventHandler {
-        @Override
-        public void handleServerEvent(ISFSEvent event) throws SFSException {
-            throw new SFSException("not used");
+    private void accumulate(String cmd, List<User> reciepients, ISFSObject params, boolean udp) {
+        if (ignoreAccumulate(cmd)) return;
+        String log = String.format("%sSEND[#%s]: %s(%d bytes)", udp ? "U" : "", reciepients.size(), cmd, params.toBinary().length);
+        trace(log);
+    }
+
+    private void accumulate(String cmd, User reciepient, ISFSObject params, boolean udp) {
+        if (ignoreAccumulate(cmd)) return;
+        String log = String.format("%sSEND[%s]: %s(%d bytes)", udp ? "U" : "", reciepient.getName(), cmd, params.toBinary().length);
+        trace(log);
+    }
+
+    private boolean ignoreAccumulate(String cmd) {
+        return DONT_ACCUMULATE.contains(cmd);
+    }
+
+    private void ignoreAccumulates() {
+        String ignoreAccumulates = getConfigProperty("silent_events");
+        if (ignoreAccumulates != null) {
+            String[] tokens = ignoreAccumulates.split("\\s*,\\s*");
+            trace("  Silent events: " + Arrays.toString(tokens));
+            Collections.addAll(DONT_ACCUMULATE, tokens);
         }
     }
 }
